a21cd76263c8a09aa7de19c373472ca30878da0c
Christian <christianramisespacuan@gmail.com>
2026-08-22 23:19:34 +0800

    My first C++ program: Hello World

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "My name is Christian" << std::endl;
+    std::cout << "I am learning C++" << std::endl;
+    return 0;
+}
